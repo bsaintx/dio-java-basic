c152899b6912fbcd26c8a4dc19d87b3ae794fcd9
bsaintx <santos.bruno.coutinho@gmail.com>
2023-08-23 19:14:08 -0300

    type of variable

--- type-variables/src/TypeVariable.java
+++ type-variables/src/TypeVariable.java
@@ -1,5 +1,14 @@
 public class TypeVariable {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        byte age = 123;
+        short year = 2021;
+        int postalCode = 24346675;
+        long cpf = 11122233396L; // Terminar com L quando for long
+        float pi = 3.12F; // terminar com F quando for float
+        double salary = 1500.64;
+
+        short shortNumber = 1;
+        int normalNumber = shortNumber;
+        short normalNumber2 = (short) normalNumber; // cast - adapta o tipo de uma variável: Pouco usado.
     }
 }
